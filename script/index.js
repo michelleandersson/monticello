78afd8c025f1cc8cbac139eb95336a2a60832d74
michelleandersson <michelleandersson@live.com>
2020-05-14 10:43:40 +0200

    finish news background and title

--- script/index.js
+++ script/index.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
     $('.header__slick-slider').slick({
       dots: true
     });
+    $('.news__slick-slider').slick({
+      centerMode: true,
+      arrows: true, 
+      dots: true, 
+      centerPadding: '60px',
+      slidesToShow: 3,
+    });
   });
+
+
+  
