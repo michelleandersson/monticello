b8a395ea85d5117452fd02756f6a07a36f6eaeb1
michelleandersson <michelleandersson@live.com>
2020-05-15 13:09:51 +0200

    finish footer

--- script/index.js
+++ script/index.js
@@ -334,23 +334,24 @@ let dotposition=email.lastIndexOf(".");
 
 if (name == "") {
   alert("Name must be filled out"); 
+  document.myform.name.style.borderColor = "red";
   document.myform.name.focus(); 
   event.preventDefault();
-  return false; 
+  //return false; 
 } 
-if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
   alert("Please enter a valid e-mail address" ); 
-  document.myform.email.focus();  
+  document.myform.email.style.borderColor = "red";
+  document.myform.email.focus();   
   event.preventDefault();
-  return false;  
-} else {
-  $(".contact__form").remove(); 
+  //return false;  
+  //the success alert does not work
+} else /*if (name==true || email==true)*/ {
+  //$(".contact__form").remove(); 
   alert("Thank you for submitting"); 
-  return true;
-
+  //return true;
 }
- 
-  
+
 } 
 
 
