f4eac3996975391426a0238e9ce0753613dd1384
michelleandersson <michelleandersson@live.com>
2020-05-15 19:44:53 +0200

    fix nav buttons

--- script/navbar.js
+++ script/navbar.js
@@ -1,25 +1,30 @@
-let aboutUs = document.getElementById("nav-about-us"); 
-let projects = document.getElementById("nav-projects"); 
-let news = document.getElementById("nav-news"); 
-let contact = document.getElementById("nav-contact"); 
+
 
 $(document).ready(function(){
     $("#nav-about-us").click(function(){
-        $("#navbar a").removeClass("nav-active");
-        $("#nav-about-us").removeClass("dot");  
-        $("#nav-about-us").addClass("nav-active"); 
+        document.getElementById("circle1").style.fontWeight = "700"; 
+        document.getElementById("circle2").style.fontWeight = "400";
+        document.getElementById("circle3").style.fontWeight = "400";
+        document.getElementById("circle4").style.fontWeight = "400";
+         
     }); 
     $("#nav-projects").click(function(){
-        $("#navbar span").removeClass("nav-active"); 
-        $("#nav-projects").addClass("nav-active"); 
+        document.getElementById("circle1").style.fontWeight = "400"; 
+        document.getElementById("circle2").style.fontWeight = "700";
+        document.getElementById("circle3").style.fontWeight = "400";
+        document.getElementById("circle4").style.fontWeight = "400";
     }); 
     $("#nav-news").click(function(){
-        $("#navbar span").removeClass("nav-active"); 
-        $("#nav-news").addClass("nav-active"); 
+        document.getElementById("circle1").style.fontWeight = "400"; 
+        document.getElementById("circle2").style.fontWeight = "400";
+        document.getElementById("circle3").style.fontWeight = "700";
+        document.getElementById("circle4").style.fontWeight = "400";
     }); 
     $("#nav-contact").click(function(){
-        $("#navbar span").removeClass("nav-active"); 
-        $("#nav-contact").addClass("nav-active"); 
+        document.getElementById("circle1").style.fontWeight = "400"; 
+        document.getElementById("circle2").style.fontWeight = "400";
+        document.getElementById("circle3").style.fontWeight = "400";
+        document.getElementById("circle4").style.fontWeight = "700";
     }); 
-}); 
+});
 
